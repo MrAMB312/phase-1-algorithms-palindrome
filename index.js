55186f4358cb2a078f5c87284f9eeb00b36e810c
MrAMB312 <a.blomarz@gmail.com>
2024-11-16 15:33:34 -0600

    complete phase 1 algorithms palindrome

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordArray = word.split('');
+  wordArray.reverse();
+  const wordBackwards = wordArray.join('');
+  return word === wordBackwards;
 }
 
 /* 
-  Add your pseudocode here
+
+1. define variable for "word" backwards (wordBackwards)
+  - 1a. split "word" into an array, turning each word into an element
+  - 1b. reverse the order of the new word array
+  - 1c. rejoin the individual letters and define as variable
+
+2. write boolean expression for word and wordBackwards
+
 */
 
 /*
-  Add written explanation of your solution here
+
+Initially, I knew the argument "word" would have to be read backwards and compared
+against its original form using the strict equality operator. I wasn't entirely sure
+how the initial string could be interpreted backwards, but with a quick Google search,
+the three steps above (1a, 1b, and 1c in my pseudocode) became clear.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +35,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("civic"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("coffee"));
 }
 
 module.exports = isPalindrome;
